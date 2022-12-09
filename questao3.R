162fc3a74a62b1f45d8ca597752fd1ddc0adcd7e
gflad <gustavofladeira@hotmail.com>
2022-12-08 23:08:17 -0300

    Update questao3.R

--- questao3.R
+++ questao3.R
@@ -1,5 +1,45 @@
+library(readr)
+library(tidyverse)
+
 setwd("C:\\Users\\gusta\\OneDrive\\FEARP\\2º semestre\\APS\\prova\\dados")
 
-library(readr)
+BF <- read_dta("prova_aps_2022_BF.dta")
 
+########################################################
+################## AJUSTANDO OS DADOS ##################
+########################################################
 
+# vetor de variaveis de identificacao
+identificacao <- c("UPA", "Estrato", "V1008", "V1014")
+# vetor de variaveis confounders
+covariadas <- c("V1022", "V2009", "V2010", "V5002A", "V5002A2",
+                "VD3004", "VD4001", "VD4002", "VD4017", "VD5005")
+# variaveis extras criadas
+extras <- c("dom_rec_bf", "total_bf", "bf_pc")
+
+# selecionando colunas
+dados <- BF %>% 
+  # somente em idade ativa (14 a 65 anos [ou 60?])
+  filter(V2009 %in% c(14:65)) %>%
+  select(identificacao, covariadas, extras)
+
+# renomeando colunas
+dados <- dados %>% 
+  rename(urbano = V1022,
+         idade = V2009,
+         negro = V2010,
+         recebeu_bf = V5002A,
+         valor_recebido_bf = V5002A2,
+         educacao = VD3004,
+         economicamente_ativa = VD4001,
+         ocupada = VD4002,
+         renda = VD4017,
+         renda_pc = VD5005)
+
+# redefinindo valores
+dados <- dados %>% 
+  mutate(urbano = ifelse(urbano == 1, 1, 0),
+         negro = ifelse(negro %in% c(2, 3, 4, 5), 1, 0),
+         recebeu_bf = ifelse(recebeu_bf == 1, 1, 0),
+         economicamente_ativa = ifelse(economicamente_ativa == 1, 1, 0),
+         ocupada = ifelse(ocupada == 1, 1, 0))
